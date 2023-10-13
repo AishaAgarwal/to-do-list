6eec7554fa73c45f71b7d7a59a86e09574b8f1c8
AishaAgarwal <aishaagarwal19567@gmail.com>
2023-10-13 19:19:51 +0530

    added the function to set deadline as well

--- script.js
+++ script.js
@@ -8,18 +8,22 @@ class todo_class{
 
     add(){
         const todoinput = document.querySelector("#myInput").value;
-        if (todoinput == ""){
-            alert("You did not enter any item!")
+        const priority = document.querySelector("#priority").value;
+        const deadline = document.querySelector("#deadline").value;
+        if (todoinput == "" || deadline == ""){
+            alert("Either You did not enter any item or the deadline is not selected for the task!")
     }
         else{
             const todoobject = {
                 id : todoobjectlist.length,
                 todotext : todoinput,
                 isdone : false,
+                deadline : deadline,
             }
         todoobjectlist.unshift(todoobject); // unshift is used instead of push because most recent entries are required on top
         this.display();
         document.querySelector("#myInput").value = ' ';
+        document.querySelector("#deadline").value = ' ';
         }
     }
 
@@ -36,19 +40,19 @@ class todo_class{
     }
     display(){
         this.ulElement.innerHTML = ""; // for cleaning the input area in the beginnig
+
+        
+
         todoobjectlist.forEach((object_item) => {
             const liElement = document.createElement("li");
             const delBtn = document.createElement("i"); // for trash icon
     
-            liElement.innerText = object_item.todotext;
+            liElement.innerText = `${object_item.todotext} (Deadline: ${object_item.deadline})`; // Display the deadline
             liElement.setAttribute("data-id", object_item.id);
     
             delBtn.setAttribute("data-id", object_item.id);
             delBtn.classList.add("far","fa-trash-alt");
 
-           
-           
-    
             liElement.appendChild(delBtn);
     
             delBtn.addEventListener("click",function(e) {
@@ -70,11 +74,11 @@ class todo_class{
     }
 }
 
+
 const listSection = document.querySelector("#mytasks");
 mytodolist = new todo_class(listSection);
 
 document.querySelector(".addbtn").addEventListener("click", function() {
     mytodolist.add()
 })
-
 
